22fed07f6869bdee2d39d6f046edc6a3f7b0a1a1
Marissa <marissabellusci@gmail.com>
2020-08-29 10:28:18 -0600

    set up helper functions

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import { getFunName } from '../helpers';
+ 
 class StorePicker extends React.Component{
     render(){
         return (
         <form className='store-selector'> 
         {/*this is a comment*/}
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name"></input>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}></input>
         <button type="submit">Visit Store!</button>
 
         </form>
